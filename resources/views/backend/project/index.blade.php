b8ae1e005b9e3d613534ee858a10fef205ae3f62
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-07-04 16:16:12 +0800

    Fix on Project's index page

--- resources/views/backend/project/index.blade.php
+++ resources/views/backend/project/index.blade.php
@@ -41,7 +41,7 @@
                                 @foreach ($projects as $project)
                                     <tr>
                                         <td>{{ $project->name }}</td>
-                                        @if (count($project->customer))
+                                        @if ($project->customer->count())
                                             <td>{!! $project->customer->trashed() ? "<label class='badge badge-danger' style='font-size: 10px;'>Customer was deleted</label>" : $project->customer->name !!}</td>
                                         @else
                                             <td>
